3fc40e687f1ea53c056deaeb8f381937bc008644
Natália Lavra <natalia.lavra@hotmail.com>
2020-07-01 20:07:37 -0300

    Adicionando nome de jogador

--- js/app.js
+++ js/app.js
@@ -1,17 +1,29 @@
-// let nomeAlunos = ["Lucas", "João","Pedro"]
+element("botaoJogar").onclick = () => {
+    const jogadorCara = element("jogadorCara").value;
+    const jogadorCoroa = element("jogadorCoroa").value;
+    
+    if (jogadorCara == "" || jogadorCoroa == ""){
+        return alert("Digite os nomes!!")
+    }
+    
+    const valor = Math.random();
 
-// for(let i = 0; i < nomeAlunos.length; i++){
-//     console.log("Nome do aluno(a): " + nomeAlunos[i]);
-// }
-
-document.getElementById("botaoJogar").onclick = () => {
-    let valor = Math.random();
     let imagem = "";
+    let nomeVencedor = "";
+
     if(valor > 0.5){
         imagem = "coroa";
+        nomeVencedor = jogadorCoroa;
     } else{
         imagem = "cara";
+        nomeVencedor = jogadorCara;
     }
-    let caminhoImagem = `assets/images/${imagem}.jpg`
-    document.getElementById("imagemMoeda").src = caminhoImagem;
+
+    const caminhoImagem = `assets/images/${imagem}.jpg`
+    element("imagemMoeda").src = caminhoImagem;
+    alert(nomeVencedor);
+}
+
+function element(id){
+    return document.getElementById(id);
 }
